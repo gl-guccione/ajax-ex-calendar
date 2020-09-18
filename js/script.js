43fb195ee1ff9436bd77188e960ab1a624967675
gl-guccione <g.guccione3@gmail.com>
2020-09-18 22:08:00 +0200

    init
    Init

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Creiamo un calendario dinamico con le festività.
+// Il calendario partirà da gennaio 2018 e si concluderà a dicembre 2018 (unici dati disponibili sull’API).
+
+// Milestone 1
+
+// Creiamo il mese di Gennaio, e con la chiamata all'API inseriamo le festività.
+
+// API: https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0
